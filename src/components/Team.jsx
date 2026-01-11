a8a7e1d5315fbc980e6d789306d1dc5da5dd21b3
Swarnadeep Saha Poddar <isspoddar@gmail.com>
2026-01-11 15:58:06 +0530

    Added Team Pages

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,21 +1,118 @@
-import React from "react";
-import MascotVision from "../assets/mascot_vision.png";
-import { Rocket } from 'lucide-react';
-import RocketVision from "../assets/rocket_vision.png";
-const events = [
-    { id: 1, title: 'Event 6', side: 'right', description: 'Final Output' },
-    { id: 2, title: 'Event 1', side: 'left', description: 'Initial Process' },
-    { id: 3, title: 'Event 2', side: 'right', description: 'Data Processing' },
-    { id: 4, title: 'Event 3', side: 'left', description: 'Validation' },
-    { id: 5, title: 'Event 4', side: 'right', description: 'Transformation' },
-    { id: 6, title: 'Event 5', side: 'left', description: 'Storage' },
+import { useState } from "react";
+import { Github, Linkedin } from "lucide-react";
+
+import teamMembers from "../data/teamMembers";
+
+const positions = [
+  "Senior Associate Member",
+  "Associate Member",
+  "Junior Associate Member",
 
 ];
+
 export default function Team() {
-    return (
-        <div className="w-11/12 mx-auto  text-center border border-amber-50 rounded-2xl mt-10 lg:mt-20">
-            <p className=" text-[80px] md:text-[110px] lg:text-[130px] font-extrabold tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-400/30 to-purple-500 select-none pointer-events-none z-0">OUR TEAM</p>
-        
-        </div>
-    );
+  const [selectedPosition, setSelectedPosition] = useState("Senior Associate Member");
+
+  const filteredMembers =
+    selectedPosition === "All"
+      ? teamMembers
+      : teamMembers.filter(
+          (member) => member.position === selectedPosition
+        );
+
+  return (
+    <section className="relative isolate min-h-screen w-11/12 mx-auto  text-center text-white px-6 py-12 pointer-events-auto text-center border border-amber-50 rounded-2xl mt-10 lg:mt-20">
+
+            <p className=" text-[60px] md:text-[110px] lg:text-[130px] font-extrabold tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-400/30 to-purple-500 select-none pointer-events-none z-0 text-center">Team</p>
+           
+
+      {/* POSITION FILTER */}
+      <div className="relative z-30 pointer-events-auto mb-12 flex justify-center border rounded-2xl">
+        {positions.map((position) => (
+          <button
+            type="button"
+            key={position}
+            onClick={() => {
+              console.log("clicked:", position);
+              setSelectedPosition(position);
+            }}
+            className={`px-5 py-2 rounded-8xl cursor-pointer 
+              ${
+                selectedPosition === position
+                  ? "primary-text-color"
+                  : ""
+              }`}
+          >
+            {position}
+          </button>
+        ))}
+      </div>
+
+      {/* TEAM GRID */}
+      <div className="relative z-10 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+        {filteredMembers.map((member, idx) => (
+          <div
+            key={idx}
+            className="  rounded-xl p-5 text-center transition hover:scale-[1.03]"
+          >
+            <img
+              src={member.photo}
+              alt={member.name}
+              className="w-32 h-32 mx-auto rounded-full object-cover mb-4"
+            />
+
+            <h3 className="text-lg font-semibold">
+              {member.name}
+            </h3>
+
+            <p className="text-sm text-purple-400">
+              {member.position}
+            </p>
+
+            <p className="text-xs text-gray-400 mb-4">
+              {member.team}
+            </p>
+
+<div className="relative z-10 flex justify-center gap-6">
+  {member.github && (
+    <a
+      href={member.github}
+      target="_blank"
+      rel="noreferrer"
+      className="group"
+      aria-label="GitHub"
+    >
+      <Github
+        size={20}
+        className="text-gray-400 transition 
+                   group-hover:text-purple-400 
+                   group-hover:scale-110"
+      />
+    </a>
+  )}
+
+  {member.linkedin && (
+    <a
+      href={member.linkedin}
+      target="_blank"
+      rel="noreferrer"
+      className="group"
+      aria-label="LinkedIn"
+    >
+      <Linkedin
+        size={20}
+        className="text-gray-400 transition 
+                   group-hover:text-purple-400 
+                   group-hover:scale-110"
+      />
+    </a>
+  )}
+</div>
+
+          </div>
+        ))}
+      </div>
+
+    </section>
+  );
 }
---
+++ src/data/teammembers.js
@@ -0,0 +1,142 @@
+import arnav from "../assets/Team/133195368 - Arnav Biswas.jpeg";
+import debashish from "../assets/Team/debas - Debashish Sinha.jpg";
+import ayush from "../assets/Team/IMG-20250409-WA0013(1) (1) - Ayush Sharma.jpg";
+import avigyan from "../assets/Team/IMG_20260104_234040 - Avigyan Guha.jpg";
+import suryashis from "../assets/Team/20250424_113409 - Suryashis Banerjee.jpg";
+import kelvin from "../assets/Team/133195368 - Arnav Biswas.jpeg";
+import surajit from "../assets/Team/IMG_20240327_151150 - Surajit Roy.jpg";
+import payel from "../assets/Team/PN.jpg";
+import payal from "../assets/Team/IMG_20250810_203410 - Payal Vyas.jpg";
+import gautam from "../assets/Team/IMG_20251225_131903 - Gautam Raj.png";
+import sandip from "../assets/Team/IMG_20260106_004229 - Sandip Basak.jpg";
+import prithivi from "../assets/Team/PSR.jpg";
+import shreyasi from "../assets/Team/SD.jpg";
+import banashree from "../assets/Team/photo-output - Banashree Barman.jpeg";
+import nilkamal from "../assets/Team/NK.jpg";
+
+
+
+const teamMembers = [
+  {
+    name: "Arnav Biswas",
+    team: "Social Media & Content Creation",
+    position: "Senior Associate Member",
+    github: "https://github.com/ArnavBiswas",
+    linkedin: "https://www.linkedin.com/in/arnav-biswas-663775306",
+    photo: arnav,
+  },
+  {
+    name: "Debashish Sinha",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "https://github.com/Debashich",
+    linkedin: "https://www.linkedin.com/in/sinhadebashish/",
+    photo: debashish,
+  },
+  {
+    name: "Ayush Sharma",
+    team: "Social Media & Content Creation",
+    position: "Junior Associate Member",
+    github: "https://github.com/AYUSH4951",
+    linkedin: "https://www.linkedin.com/in/ayush-sharma-4a2849323",
+    photo: ayush,
+  },
+  {
+    name: "Avigyan Guha",
+    team: "Social Media & Content Creation",
+    position: "Junior Associate Member",
+    github: "https://github.com/Archivefloyd",
+    linkedin: "https://www.linkedin.com/in/avigyan-guha-a16835323",
+    photo: avigyan,
+  },
+  {
+    name: "Suryashis Banerjee",
+    team: "Management",
+    position: "Senior Associate Member",
+    github: "https://github.com/Suryashis4",
+    linkedin: "https://www.linkedin.com/in/suryashis-banerjee-847614265",
+    photo: suryashis,
+  },
+  {
+    name: "Kelvin Linus",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "https://github.com/KelvinLinus07",
+    linkedin: "https://www.linkedin.com/in/kelvin-linus-704578312",
+    photo: kelvin,
+  },
+  {
+    name: "Banashree Barman",
+    team: "Social Media & Content Creation",
+    position: "Junior Associate Member",
+    github: "https://github.com/BoniBTech",
+    linkedin: "https://www.linkedin.com/in/banashree-barman-2863a0359",
+    photo: banashree,
+  },
+  {
+    name: "PRITHIVI RAJ SHA",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/prithivi-raj-sha-741669309",
+    photo: prithivi,
+  },
+  {
+    name: "Shreyasi Dutta",
+    team: "Technical",
+    position: "Junior Associate Member",
+    github: "https://github.com/Shreyasi53",
+    linkedin: "https://www.linkedin.com/in/shreyasi-duttaaa/",
+    photo: shreyasi,
+  },
+  {
+    name: "Surajit Roy",
+    team: "Technical",
+    position: "Junior Associate Member",
+    github: "https://github.com/Surajit1410",
+    linkedin: "https://www.linkedin.com/in/surajit-roy-36697432a",
+    photo: surajit,
+  },
+  {
+    name: "Sandip Basak",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/sandip-basak-738933288",
+    photo: sandip,
+  },
+  {
+    name: "Payel Nunia",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/payel-nunia-b09087326",
+    photo: payel,
+  },
+  {
+    name: "Gautam Raj",
+    team: "Management",
+    position: "Associate Member",
+    github: "https://github.com/Gautam2938",
+    linkedin: "https://in.linkedin.com/in/gautam-raj-",
+    photo: gautam,
+  },
+  {
+    name: "Nilkamal Adhikari",
+    team: "Technical",
+    position: "Junior Associate Member",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/nilkamal-adhikari-210592323",
+    photo: nilkamal,
+  },
+  {
+    name: "Payal Vyas",
+    team: "Management",
+    position: "Junior Associate Member",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/payal-vyas-900801315",
+    photo: payal,
+  },
+];
+
+export default teamMembers;
